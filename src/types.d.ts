fa00988fe00d4cea01cf23ca6fafc20df35c7583
AlessandroMochiOnori <alessandromochionori@gmail.com>
2026-04-24 17:45:34 +0200

    ciao

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,9 @@
+declare module '*.css';
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
 declare module '*.JPG' {
   const content: { src: string };
   export default content;
